9b6a908b1d76e6b18b9f7ddacdbdb778badf9f39
Emanuele Buchicchio <emanbuc@emanuelebuchicchio.com>
2023-06-05 17:58:27 +0000

    status, metrics e log

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,42 @@
 from flask import Flask
+from flask import json
+
+import logging
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "ciaone!"
 
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!!!'
+
+@app.route('/status')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info("hanno chiamato status")
+
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23, "jobNumber": 40}}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info("richiesta metric completata con successo")
+    return response
 if __name__ == "__main__":
+    
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    
     app.run(host='0.0.0.0')
+
